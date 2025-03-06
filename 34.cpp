000235e70838e873b0353d2e62342d08c16efd79
bugOpsX <sumantkumar3172@gmail.com>
2025-03-06 19:52:50 +0530

    basics of C++

--- 34.cpp
+++ 34.cpp
@@ -3,5 +3,38 @@
 #include<iostream>
 using namespace std;
 
+#define pi 3.14
 
+float area(float radiusOfCircle)
+{
+    return pi*radiusOfCircle*radiusOfCircle;
+}
 
+int area(int lenghtRectangle, int breadthRectangle)
+{
+    return lenghtRectangle*breadthRectangle;
+}
+
+float area(float baseTriangle, float heightTriangle)
+{
+    return 0.5*baseTriangle*heightTriangle;
+}
+
+int main()
+{
+    int rc, lr, br, bt, ht;
+
+    cout<<"Enter the Radius of Circle: ";
+    cin>>rc;
+    cout<<"The Area of Circle is: "<<area(rc)<<endl;
+
+    cout<<"Enter the Length and Breadth of Rectangle: ";
+    cin>>lr>>br;
+    cout<<"The Area of Rectangle is: "<<area(lr, br)<<endl;
+
+    cout<<"Enter the Base and Height of Triangle: ";
+    cin>>bt>>ht;
+    cout<<"The Area of Triangle is: "<<area(bt,ht)<<endl;
+
+    return 0;
+}
