c598b76238850ff223e9a4817302a4fa787d6669
bugOpsX <sumantkumar3172@gmail.com>
2025-03-13 17:53:19 +0530

    Information program using variables

---
+++ PYTHON/1.py
@@ -0,0 +1,20 @@
+#Use of variables variables (String, Float, Integer, Boolean) in Simple Program.
+
+Student_name = "Sumant Kumar"
+Enrollment = "XXXXXX502"
+Division = "2EN6"
+Age = 21
+GPA = 8.95
+is_pass = True
+
+print("Student's Information:")
+print(f"Name: {Student_name}")
+print(f"Enrollment: {Enrollment}")
+print (f"Division: {Division}")
+print (f"Age: {Age}")
+print(f"GPA: {GPA}")
+
+if is_pass:
+    print("He passed in SEM exam.")
+else:
+    print("He FAILED in exam.")
