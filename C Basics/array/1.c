7c8b41a7e12166e9c415ecc52e8fa376308a46b5
Sumant Kumar <sumantkumar3172@gmail.com>
2025-07-29 17:47:26 +0530

    Fix loop condition in array printing program for clarity

--- C Basics/array/1.c
+++ C Basics/array/1.c
@@ -4,7 +4,7 @@
 
 int main(){
     int arr[] = {10,14,12};
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+    for(int i = 0; i < 3; i++){
         printf("%d\n", arr[i]);
     }
     return 0;
