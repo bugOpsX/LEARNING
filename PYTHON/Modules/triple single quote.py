84b76a1b56e19c88bed63515dbe1e7ca4748c448
bugOpsX <sumantkumar3172@gmail.com>
2025-09-01 10:20:35 +0530

    multiline string using triple single quotes

---
+++ PYTHON/Modules/triple single quote.py
@@ -0,0 +1,22 @@
+# write a program to print using ' ' ' (triple single quote)
+
+print('''WHEREAS I did not desire in childhood to be a part of this
+but desired most of all to be a part. A piece combined with
+others to make up a whole. Some but not all of something.
+In Lakota it’s onspa, a piece or part of anything. Like the
+creek trickling behind my aunt’s house where Uncle built her
+a bridge to cross from bank to bank, not far from a grassy
+clearing with three tipis, a place to gather. She holds three-
+day workshops on traditional arts, young people from Kyle
+and Potato Creek arrive one by one eager to participate.
+They have the option my auntie says to sleep at home and
+return in the morning but by and large they’ll stay and camp
+even during South Dakota winters. The comfort of being to-
+gether. I think of Plains winds snow drifts ice and limbs the
+exposure and when I slide my arms into a wool coat and
+put my hand to the door knob, ready to brave the sub-zero
+dark, someone says be careful out there always consider the
+snow your friend. Think badly of it, snow will burn you. I
+walk out remembering that for millennia we have called
+ourselves Lakota meaning friend or ally. This relationship
+to the other. Some but not all, yet our piece connecting;''')
