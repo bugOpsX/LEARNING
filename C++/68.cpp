e6dd0e535cac93efc94434e6b14e5dabcd6c9368
Raunak Tyagi <raunaktyagi.05207@gmail.com>
2025-10-27 18:38:34 +0530

    Initial commit

---
+++ C++/68.cpp
@@ -0,0 +1,85 @@
+//Write a C++ program to implement a singly linked list and reverse it iteratively.
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+    
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+    
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(value);
+        
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    
+    void reverse() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+        
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        head = prev;
+    }
+    
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data;
+            if (temp->next != nullptr) {
+                cout << " -> ";
+            }
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+void main() {
+    LinkedList list;
+    
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.insertAtEnd(40);
+    list.insertAtEnd(50);
+    
+    cout << "Original List: ";
+    list.display();
+    
+    list.reverse();
+    
+    cout << "Reversed List: ";
+    list.display();
+}
